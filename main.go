6acfe6cf5833a829603c8985c351938fcce75979
darts <senan.dart@gmail.com>
2023-03-09 18:03:44 +0000

    Mostly functional

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"darts/gql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -23,18 +22,23 @@ type AppStatus struct {
 	Name       string `json:"name"`
 	StatusText string `json:"statusText"`
 	Priority   int32  `json:"priority"`
+	Emoji      string `json:"emoji"`
 }
 
 type AppList struct {
 	Frequency      int32       `json:"frequency"`
 	Randomise      int32       `json:"randomise"`
 	FallbackStatus string      `json:"fallback"`
+	FallbackEmoji  string      `json:"fallback_emoji"`
 	Apps           []AppStatus `json:"apps"`
 }
 
-type UserStatus struct {
-	ClientMutationId graphql.String `json:"clientMutationId"`
-	Message          graphql.String `json:"message"`
+type ChangeUserStatusInput struct {
+	ClientMutationId string `json:"clientMutationId"`
+	// The emoji to represent your status. Can either be a native Unicode emoji or an emoji name with colons, e.g., :grinning:.
+	Emoji     string    `json:"emoji"`
+	ExpiresAt time.Time `json:"expiresAt"`
+	Message   string    `json:"message"`
 }
 
 type authedTransport struct {
@@ -44,10 +48,17 @@ type authedTransport struct {
 
 var graphqlClient *graphql.Client
 
+var statusMutation struct {
+	ChangeUserStatus struct {
+		ClientMutationId string
+		Status           struct {
+			Message string
+		}
+	} `graphql:"changeUserStatus(input: $input)"`
+}
+
 func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "bearer "+t.key)
-	// req.Header.Set("X-Github-Next-Global-ID", "1")
-	// req.Header.Set("Content-Type", "application/json")
 	return t.wrapped.RoundTrip(req)
 }
 
@@ -124,22 +135,17 @@ func parseAppsFromFile() *AppList {
 	return &apps
 }
 
-func writeToGithubStatus(str string, expiry time.Time) bool {
-	// fmt.Printf("Writing to Github status: %s\n", str)
-	// gqlReq := gql.ChangeUserStatusInput{ClientMutationId: "darts/status", Message: str, ExpiresAt: expiry}
-	// res, err := gql.UpdateStatus(context.Background(), graphqlClient, gqlReq)
-	// fmt.Println(res)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Call to Github failed: %s\n", err)
-	// }
-	// return err == nil
-	return true
+func writeToGithubStatus(str string, emoji string, expiry time.Time) bool {
+	fmt.Printf("Writing to Github status: %s\n", str)
+	gqlReq := map[string]interface{}{
+		"input": ChangeUserStatusInput{ClientMutationId: "darts/status", Message: str, Emoji: emoji, ExpiresAt: expiry},
+	}
+	err := graphqlClient.Mutate(context.Background(), &statusMutation, gqlReq)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Call to Github failed: %s\n", err)
+	}
+	return err == nil
 }
-
-// func manWriteToGithubStatus(str string) bool {
-// 	client := graphql.NewClient("https://machinebox.io/graphql")
-
-// }
 
 func getAppMap(sortedArr []AppStatus) map[int32][]AppStatus {
 	retMap := map[int32][]AppStatus{}
@@ -153,14 +159,14 @@ func getAppMap(sortedArr []AppStatus) map[int32][]AppStatus {
 	return retMap
 }
 
-func getCurrentApp(activeApps map[string]bool, appList []AppStatus, appMap map[int32][]AppStatus, fallback string) string {
+func getCurrentApp(activeApps map[string]bool, appList []AppStatus, appMap map[int32][]AppStatus, config AppList) (string, string) {
 	for _, app := range appList {
 		appIsRunning := activeApps[app.Name]
 		if appIsRunning {
-			return app.StatusText
+			return app.StatusText, app.Emoji
 		}
 	}
-	return fallback
+	return config.FallbackStatus, config.FallbackEmoji
 }
 
 func manageStatus() {
@@ -170,9 +176,9 @@ func manageStatus() {
 	appMap := getAppMap(appList)
 	for true {
 		activeApps := toHashSet(toSingletonArray(getRunningApps()))
-		curStr := getCurrentApp(activeApps, appList, appMap, appConfig.FallbackStatus)
+		curStr, curEmoji := getCurrentApp(activeApps, appList, appMap, *appConfig)
 		expiry := time.Now().Add(time.Duration(appConfig.Frequency) * time.Second * 3) // expire after 3 missed updates
-		writeToGithubStatus(curStr, expiry)
+		writeToGithubStatus(curStr, curEmoji, expiry)
 		time.Sleep(time.Duration(appConfig.Frequency) * time.Second)
 	}
 }
@@ -181,7 +187,7 @@ func resetOnClose() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	<-sig // buffered channels block if empty
-	writeToGithubStatus("", time.Now())
+	writeToGithubStatus("", "", time.Now())
 	os.Exit(0)
 }
 
@@ -200,57 +206,6 @@ func initClient() {
 	}
 
 	graphqlClient = graphql.NewClient("https://api.github.com/graphql", &httpClient)
-
-	var q struct {
-		Viewer struct {
-			Name string
-		} `graphql:"viewer"`
-	}
-
-	var m struct {
-		ChangeUserStatus struct {
-			ClientMutationId string
-			Status           struct {
-				Message string
-			}
-		} `graphql:"changeUserStatus(input: $input)"`
-	}
-
-	// input := UserStatus{ClientMutationId: *graphql.NewString("test"), Message: *graphql.NewString("test")}
-
-	// variables := map[string]interface{}{
-	// 	"input": input,
-	// }
-
-	variabless := map[string]interface{}{
-		"input": gql.ChangeUserStatusInput{ClientMutationId: "ABC", Message: "test"},
-	}
-
-	err := graphqlClient.Query(context.Background(), &q, nil)
-	if err != nil {
-		print(err)
-	}
-	print(q)
-
-	// err = graphqlClient.Mutate(context.Background(), &m, variables)
-	// if err != nil {
-	// 	print(err)
-	// }
-	// print(m)
-
-	err = graphqlClient.Mutate(context.Background(), &m, variabless)
-	if err != nil {
-		print(err)
-	}
-	print(m)
-
-	// resp, err := gql.WhoAmI(context.Background(), graphqlClient)
-	// fmt.Println(resp, err)
-
-	// input := gql.ChangeUserStatusInput{ClientMutationId: "test", Message: "test", ExpiresAt: time.Now()}
-
-	// res, er := gql.UpdateStatus(context.Background(), graphqlClient, input)
-	// fmt.Println(res, er)
 }
 
 func print(v interface{}) {
@@ -266,5 +221,5 @@ func main() {
 	go resetOnClose()
 
 	initClient()
-	// manageStatus()
+	manageStatus()
 }
